b2a6530ace07265eadf55a0cd57123acd96ebbcc
TheScrewdriver <kinoide56@gmail.com>
2022-09-10 11:56:19 +0200

    ft_memmove

--- srcs/strings/ft_memmove.c
+++ srcs/strings/ft_memmove.c
@@ -6,43 +6,26 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 10:43:29 by rbroque           #+#    #+#             */
-/*   Updated: 2022/09/09 11:49:09 by rbroque          ###   ########.fr       */
+/*   Updated: 2022/09/10 11:53:01 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_strings.h"
 
-static size_t	get_min(size_t size1, size_t size2)
-{
-	if (size1 > size2)
-		return (size2);
-	return (size1);
-}
-
-#include <stdio.h>
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	uint8_t	buffer[BUFFER_SIZE];
 	size_t	i;
 
-	ft_bzero(buffer, BUFFER_SIZE);
-	i = 0;
-	while (i < n)
+	if (src < dest && dest < src + n)
 	{
-		ft_memcpy(buffer, src + i, BUFFER_SIZE);
-		printf("----------->%zu\n", get_min(n - i, BUFFER_SIZE));
-		ft_memcpy(dest + i, buffer, get_min(n - i, BUFFER_SIZE));
-		ft_bzero(buffer, BUFFER_SIZE);
-		i += BUFFER_SIZE;
+		i = 0;
+		while (i < n)
+		{
+			((uint8_t *)dest)[n - i - 1] = ((uint8_t *)src)[n - i - 1];
+			++i;
+		}
 	}
-	printf("\n\n");
+	else
+		ft_memcpy(dest, src, n);
 	return (dest);
 }
-
-/*
-Start stop
-
-Start
-
-StStarttop
-*/
--- tests/srcs/strings/ft_memmove.test.c
+++ tests/srcs/strings/ft_memmove.test.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 10:49:42 by rbroque           #+#    #+#             */
-/*   Updated: 2022/09/09 11:37:00 by rbroque          ###   ########.fr       */
+/*   Updated: 2022/09/10 11:55:29 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ START_TEST(int_array)
 	const size_t	dest_size = 5;
 	int				src[] = {1, 2, 3, 4, 5};
 	int				dest[5];
-	int				ft_src[] = {1, 2, 3, 4 ,5};
+	int				ft_src[] = {1, 2, 3, 4, 5};
 	int				ft_dest[5];
 
 	memmove(dest, src, size * sizeof(int));
@@ -53,8 +53,21 @@ START_TEST(overlap)
 
 	memmove(src + 2, src, size * sizeof(char));
 	ft_memmove(ft_src + 2, ft_src, size * sizeof(char));
-	printf("src -----> %s; dest ------> %s\n", src , dest);
-	printf("ft_src --> %s; ft_dest ---> %s\n", ft_src , ft_dest);
+	ck_assert_mem_eq(dest, ft_dest, dest_size);
+}
+END_TEST
+
+START_TEST(empty_string)
+{
+	const size_t	size = 1;
+	const size_t	dest_size = 1;
+	char			src[] = "";
+	char			*dest = src;
+	char			ft_src[] = "";
+	char			*ft_dest = ft_src;
+
+	memmove(src, src, size * sizeof(char));
+	ft_memmove(ft_src, ft_src, size * sizeof(char));
 	ck_assert_mem_eq(dest, ft_dest, dest_size);
 }
 END_TEST
@@ -84,6 +97,7 @@ Suite	*ft_memmove_suite(void)
 	tcase_add_test(new, int_array);
 	tcase_add_test(new, char_array);
 	tcase_add_test(new, overlap);
+	tcase_add_test(new, empty_string);
 	tcase_add_test(new, null_size);
 	suite_add_tcase(s, new);
 	return (s);
