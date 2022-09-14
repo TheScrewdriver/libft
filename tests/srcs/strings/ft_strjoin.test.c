3a43106927419da3a99e6e3cf76d39b2dc5dfc98
TheScrewdriver <kinoide56@gmail.com>
2022-09-14 18:09:01 +0200

    more test for ft_strjoin

--- tests/srcs/strings/ft_strjoin.test.c
+++ tests/srcs/strings/ft_strjoin.test.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 17:47:46 by rbroque           #+#    #+#             */
-/*   Updated: 2022/09/14 17:48:24 by rbroque          ###   ########.fr       */
+/*   Updated: 2022/09/14 18:08:31 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,49 +24,46 @@ START_TEST(same_strings)
 	free(join);
 }
 END_TEST
-/*
-START_TEST(same_beginning1)
-{
-	char	*string1 = "hello my name is Ouah";
-	char	*string2 = "hello my name is Neo";
-	size_t	size = 21;
-	int		result;
-	int		ft_result;
 
-	result = strjoin(string1, string2, size);
-	ft_result = ft_strjoin(string1, string2, size);
-	ck_assert_int_eq(are_same_sign(result, ft_result), 1);
-}
-END_TEST
-
-START_TEST(same_beginning2)
-{
-	char	*string1 = "hello my name is Neo";
-	char	*string2 = "hello my name is Ouah";
-	size_t	size = 21;
-	int		result;
-	int		ft_result;
-
-	result = strjoin(string1, string2, size);
-	ft_result = ft_strjoin(string1, string2, size);
-	ck_assert_int_eq(are_same_sign(result, ft_result), 1);
-}
-END_TEST
-
-START_TEST(empty_strings)
+START_TEST(empty_string1)
 {
 	char	*string1 = "";
 	char	*string2 = "";
-	size_t	size = 1;
-	int		result;
-	int		ft_result;
+	char	*expected_str = "";
+	char	*join;
 
-	result = strjoin(string1, string2, size);
-	ft_result = ft_strjoin(string1, string2, size);
-	ck_assert_int_eq(are_same_sign(result, ft_result), 1);
+	join = ft_strjoin(string1, string2);
+	ck_assert_str_eq(expected_str, join);
+	free(join);
 }
 END_TEST
-*/
+
+START_TEST(empty_string2)
+{
+	char	*string1 = "burger";
+	char	*string2 = "";
+	char	*expected_str = "burger";
+	char	*join;
+
+	join = ft_strjoin(string1, string2);
+	ck_assert_str_eq(expected_str, join);
+	free(join);
+}
+END_TEST
+
+START_TEST(empty_string3)
+{
+	char	*string1 = "";
+	char	*string2 = "burger";
+	char	*expected_str = "burger";
+	char	*join;
+
+	join = ft_strjoin(string1, string2);
+	ck_assert_str_eq(expected_str, join);
+	free(join);
+}
+END_TEST
+
 Suite	*ft_strjoin_suite(void)
 {
 	Suite	*s;
@@ -75,9 +72,9 @@ Suite	*ft_strjoin_suite(void)
 	s = suite_create("FT_STRJOIN");
 	new = tcase_create("Result");
 	tcase_add_test(new, same_strings);
-//	tcase_add_test(new, same_beginning1);
-//	tcase_add_test(new, same_beginning2);
-//	tcase_add_test(new, empty_strings);
+	tcase_add_test(new, empty_string1);
+	tcase_add_test(new, empty_string2);
+	tcase_add_test(new, empty_string3);
 	suite_add_tcase(s, new);
 	return (s);
 }
