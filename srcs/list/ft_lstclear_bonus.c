20bd3d2fcbf8948eb3febde59c17999cd66e49e0
romann-broque <kinoide56@gmail.com>
2022-09-21 11:17:21 +0200

    ft_lstclear now uses ft_lstdelone

--- srcs/list/ft_lstclear_bonus.c
+++ srcs/list/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 10:59:17 by rbroque           #+#    #+#             */
-/*   Updated: 2022/09/21 11:02:35 by rbroque          ###   ########.fr       */
+/*   Updated: 2022/09/21 11:16:53 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	if (lst != NULL && *lst != NULL)
 	{
 		ft_lstclear(&(*lst)->next, del);
-		if (del != NULL)
-			del((*lst)->content);
-		free(*lst);
+		ft_lstdelone(*lst, del);
 		*lst = NULL;
 	}
 }
