5d73c2a391e6621552a82ea9ef4687368f728e63
romann-broque <kinoide56@gmail.com>
2022-11-07 00:29:13 +0100

    strjoin tests

--- srcs/strings/ft_strjoin.c
+++ srcs/strings/ft_strjoin.c
@@ -6,25 +6,11 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 17:30:49 by rbroque           #+#    #+#             */
-/*   Updated: 2022/09/22 15:05:49 by rbroque          ###   ########.fr       */
+/*   Updated: 2022/11/07 00:28:08 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static char	*ft_strcpy(char *dest, char *src)
-{
-	size_t	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		++i;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -32,11 +18,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	const size_t	len2 = ft_strlen(s2);
 	char			*new;
 
-	new = (char *)malloc((len1 + len2 + 1) + sizeof(char));
+	new = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	if (new != NULL)
 	{
-		ft_strcpy(new, (char *)s1);
-		ft_strcpy(new + len1, (char *)s2);
+		ft_memcpy(new, s1, len1);
+		ft_memcpy(new + len1, s2, len2);
+		new[len1 + len2] = '\0';
 	}
 	return (new);
 }
