540a9c182a4ca64bedca8fbab69c4ccb06c1b9eb
romann-broque <kinoide56@gmail.com>
2022-11-06 19:17:43 +0100

    ft_calloc

--- srcs/memory/ft_calloc.c
+++ srcs/memory/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 16:13:54 by rbroque           #+#    #+#             */
-/*   Updated: 2022/09/14 15:30:05 by rbroque          ###   ########.fr       */
+/*   Updated: 2022/11/06 19:15:28 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	const size_t	total_size = nmemb * size;
 	void			*ptr;
 
-	ptr = malloc(total_size);
+	ptr = NULL;
+	if (nmemb < SIZE_MAX && size < SIZE_MAX)
+		ptr = malloc(total_size);
 	if (ptr != NULL)
 		ft_bzero(ptr, total_size);
 	return (ptr);
