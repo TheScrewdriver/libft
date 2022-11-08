5a5a6a7e55df175d11c715be02a8a17804a7b97b
romann-broque <kinoide56@gmail.com>
2022-11-08 01:16:09 +0100

    strjoin and substr fixes

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 17:30:49 by rbroque           #+#    #+#             */
-/*   Updated: 2022/11/07 00:28:08 by rbroque          ###   ########.fr       */
+/*   Updated: 2022/11/08 01:02:31 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,23 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	const size_t	len1 = ft_strlen(s1);
-	const size_t	len2 = ft_strlen(s2);
-	char			*new;
+	size_t	len1;
+	size_t	len2;
+	char	*new;
 
-	new = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-	if (new != NULL)
+	if (s1 == NULL || s2 == NULL)
+		new = NULL;
+	else
 	{
-		ft_memcpy(new, s1, len1);
-		ft_memcpy(new + len1, s2, len2);
-		new[len1 + len2] = '\0';
+		len1 = ft_strlen(s1);
+		len2 = ft_strlen(s2);
+		new = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+		if (new != NULL)
+		{
+			ft_memcpy(new, s1, len1);
+			ft_memcpy(new + len1, s2, len2);
+			new[len1 + len2] = '\0';
+		}
 	}
 	return (new);
 }
