883a91636b25429d46a3768ceb4babe0f8ce448a
Romann BROQUE <rbroque@e1r7p5.clusters.42paris.fr>
2022-11-11 21:36:07 +0100

    add gnl to libft

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -1,24 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/31 10:15:48 by rbroque           #+#    #+#             */
-/*   Updated: 2022/11/11 21:23:19 by rbroque          ###   ########.fr       */
+/*   Created: 2022/10/10 15:00:22 by rbroque           #+#    #+#             */
+/*   Updated: 2022/11/11 21:35:33 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-# include "ft_conv.h"
-# include "ft_ctype.h"
-# include "ft_list.h"
-# include "ft_memory.h"
-# include "ft_output.h"
-# include "ft_strings.h"
-# include "get_next_line.h"
+# include "libft.h"
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+# define OPEN_MAX FOPEN_MAX
+
+typedef enum e_line_status
+{
+	INVALID_LINE,
+	VALID_LINE
+}	t_line_status;
+
+size_t	index_of(const char *str, const char c);
+char	*strnjoin(char *s1, const char *s2, const size_t n);
+
+char	*get_next_line(int fd);
 
 #endif
